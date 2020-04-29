e1664e0dc1da94b1f52c65b42ad389a880ce4019
Christopher Alcock <christopheralcock@Christophers-MacBook-Pro-2.local>
2020-04-29 23:23:12 +0100

    reverting to a pattern that definitely worked before

---
+++ Assets/RestartButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class RestartButton : MonoBehaviour
+{
+
+    public void RestartGame()
+    {
+        foreach (var music in MusicManager.musics)
+        {
+            Destroy(music);
+        }
+
+        SceneManager.LoadScene("MusicLoader");
+
+        Debug.Log("You have clicked the button!");
+    }
+}
